e914904424b1f51f9f282e19734207762b062e73
Dmitry Kaigorodov <dima.kaigorodov@gmail.com>
2024-06-26 13:16:43 +0200

    index2 works

--- wip/index2.js
+++ wip/index2.js
@@ -5,10 +5,14 @@ async function main() {
 
   const page = await browser.newPage()
   await page.goto('https://www.ip-score.com/', { waitUntil: 'networkidle2' })
+  // await page.goto('https://www.ip-score.com/', { waitUntil: 'networkidle0' })
+  delay(500)
+  // console.log(await page.content());
   const score = await page.$eval("#score", elt => elt.innerText)
   // const score = await page.waitForSelector("#score", { visible: true, timeout: 500 })?.textContent
   // const score = await page.waitForSelector(".trustworthy-status:not(hide)", { visible: true })?.textContent
   await page.screenshot({ path: 'ip-score-100.jpg' })
+  delay(500)
 
   // Expecting '100/100'
   return score
