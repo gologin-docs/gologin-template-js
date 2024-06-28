b85807dc38efa693fee96a4b8a7634b85333f01d
Dmitry Kaigorodov <dima.kaigorodov@gmail.com>
2024-06-28 12:26:37 +0200

    minor update index2, delay

--- wip/index2.js
+++ wip/index2.js
@@ -7,14 +7,14 @@ async function main() {
   const browser = await gologin.launch()
   const page = await browser.newPage()
   await page.goto('https://iphey.com/', { waitUntil: 'networkidle2' })
-  delay(500)
+  delay()
 
   const score = await page.$eval(".trustworthy-status:not(.hide)", elt => elt?.innerText?.trim())
-  await page.screenshot({ path: 'ip-score-100.jpg' })
+  await page.screenshot({ path: 'iphey-com.jpg' })
 
   // Expecting 'Trustworthy'
   return score
 }
 
 main().catch(console.error).then(console.info).finally(gologin.exit)
-const delay = ms => new Promise(res => setTimeout(res, ms));
+const delay = (ms = 300) => new Promise(res => setTimeout(res, ms));
